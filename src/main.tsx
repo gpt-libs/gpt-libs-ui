58c5e1031c507696e1bdd786e46eccc3e1816ef6
Aubrey Sledzinski <asledzinski88@gmail.com>
2023-01-10 21:43:02 -0500

    feat: created basic layout

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
+import "./global.css";
+import {theme} from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
